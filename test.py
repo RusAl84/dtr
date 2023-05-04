27a450a94d07f23e14d03357d003fa7f75e7b821
Aleksey <rusal@bk.ru>
2023-05-04 11:02:04 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+﻿from sklearn.datasets import load_iris # примеры данных 
+from sklearn import tree # модуль для деревьев
+iris = load_iris() # загружаем данные
+X=iris.data # примеры входов
+y=iris.target # метки (примеры выходов)
+clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
+clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
+tree.plot_tree(clf) # отображаем
