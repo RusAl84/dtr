97487cc2d0129290eae9d6ed78c1d1c33639acf1
Aleksey <rusal@bk.ru>
2023-05-04 11:16:23 +0300

    add plot

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 ﻿from sklearn.datasets import load_iris # примеры данных 
 from sklearn import tree # модуль для деревьев
-iris = load_iris() # загружаем данные
-X=iris.data # примеры входов
-y=iris.target # метки (примеры выходов)
+import matplotlib.pyplot as plt
+
+# iris = load_iris() # загружаем данные
+# X=iris.data # примеры входов
+# y=iris.target # метки (примеры выходов)
 clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
 clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
+
+# plot tree
+plt.figure(figsize=(12,12))
 tree.plot_tree(clf) # отображаем
+plt.show()
