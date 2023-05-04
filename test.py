5f4b67d5ec1276e310eea6b35c367c1886a1653d
Aleksey <rusal@bk.ru>
2023-05-04 11:25:57 +0300

    fix

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 ﻿from sklearn.datasets import load_iris # примеры данных 
 from sklearn import tree # модуль для деревьев
 import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_excel('data.xlsx', index_col=0, comment='#')
+# print(df[df.columns[0:3]])
+# print(df.iloc[:, 0:3])
+X= df.iloc[:, 0:3].values.tolist()
+print(X)
 
 # iris = load_iris() # загружаем данные
 # X=iris.data # примеры входов
 # y=iris.target # метки (примеры выходов)
-clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
-clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
 
-# plot tree
-plt.figure(figsize=(12,12))
-tree.plot_tree(clf) # отображаем
-plt.show()
+# clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
+# clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
+
+# # plot tree
+# plt.figure(figsize=(12,12))
+# tree.plot_tree(clf) # отображаем
+# plt.show()
