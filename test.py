84e08bf28835d8a3739b71266f94d67b1cd9956c
Aleksey <rusal@bk.ru>
2023-05-10 14:44:59 +0300

    add data

--- test.py
+++ test.py
@@ -4,16 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_excel('data.xlsx', index_col=0, comment='#')
-# print(df[df.columns[0:3]])
-# print(df.iloc[:, 0:3])
 X= df.iloc[:, 0:4].values.tolist()
-# print(X)
 y= df.iloc[:, 4:5].values.tolist()
-
-# print(y)
-# iris = load_iris() # загружаем данные
-# X=iris.data # примеры входов
-# y=iris.target # метки (примеры выходов)
 
 clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
 clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
@@ -27,7 +19,7 @@ from sklearn.tree import export_text # подключаем функцию
 r = export_text(clf) # переводим дерево в текстовую строку
 print(r) # печатаем
 
-res= clf.predict([[182, 68, 15, 55]]) #сюда подставить свои праметры
+res= clf.predict([[165, 84, 95, 95]]) #сюда подставить свои праметры
 #Рост (см)	Вес (кг)	Сколько ехать до Стромынки (в минутах)	Сколько ехать до ЮЗ (в минутах)
 print (res)
 # ответ
