01308a0bd09c42a77b70c37146a7f2478cd3edda
Aleksey <rusal@bk.ru>
2023-05-10 15:10:41 +0300

    randomForest

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 ﻿from sklearn.datasets import load_iris # примеры данных 
 from sklearn import tree # модуль для деревьев
+from sklearn.ensemble import RandomForestClassifier # случайный лес
 import matplotlib.pyplot as plt
+
 import pandas as pd
 
 df = pd.read_excel('data.xlsx', index_col=0, comment='#')
@@ -8,6 +10,10 @@ X= df.iloc[:, 0:4].values.tolist()
 y= df.iloc[:, 4:5].values.tolist()
 
 clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
+
+clf = RandomForestClassifier(max_depth=5,# 5 максимальная глубина дерева
+                            n_estimators=5,# 10 число деревьев в лесу
+                            max_features=4)# максимальное число признаков для каждого дерева
 clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
 
 # plot tree
@@ -19,7 +25,7 @@ from sklearn.tree import export_text # подключаем функцию
 r = export_text(clf) # переводим дерево в текстовую строку
 print(r) # печатаем
 
-res= clf.predict([[165, 84, 95, 95]]) #сюда подставить свои праметры
+res= clf.predict([[176, 56, 40, 95]]) #сюда подставить свои праметры
 #Рост (см)	Вес (кг)	Сколько ехать до Стромынки (в минутах)	Сколько ехать до ЮЗ (в минутах)
 print (res)
 # ответ
