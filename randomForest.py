7dd6f084bee1cdf91ac14e1700b01a1ae35141c1
Aleksey <rusal@bk.ru>
2023-05-11 12:48:48 +0300

    todp

--- randomForest.py
+++ randomForest.py
@@ -11,20 +11,23 @@ y= df.iloc[:, 4:5].values.tolist()
 
 # clf = tree.DecisionTreeClassifier() # создаем классификатор на основе дерева
 
-num_tree = 3
-clf = RandomForestClassifier(max_depth=3,# 5 максимальная глубина дерева
+num_tree =3
+clf = RandomForestClassifier(max_depth=5,# 5 максимальная глубина дерева
                             n_estimators=num_tree,# 5 число деревьев в лесу
-                            max_features=3)# максимальное число признаков для каждого дерева
+                            max_features=4)# максимальное число признаков для каждого дерева
 clf = clf.fit(X, y) # обучаем его, т.е. создаем само дерево
+#TODO
+#fdfd fddffd 
+
 
 # plot tree
-for i in range(num_tree):
-    tree_data=clf.estimators_[i] 
-    plt.figure(figsize=(12,12))
-    tree.plot_tree(tree_data, filled=True) # отображаем
-    plt.show()
+# for i in range(num_tree):
+#     tree_data=clf.estimators_[i] 
+#     plt.figure(figsize=(12,12))
+#     tree.plot_tree(tree_data, filled=True) # отображаем
+#     plt.show()
 
-res= clf.predict([[164, 56, 90, 90]]) #сюда подставить свои праметры
+res= clf.predict([[178, 96, 45, 90]]) #сюда подставить свои праметры
 #Рост (см)	Вес (кг)	Сколько ехать до Стромынки (в минутах)	Сколько ехать до ЮЗ (в минутах)
 print (res[0])
 # ответ
